a3756e670e403ed40bc98e8f28c99d89e1c2acb8
Nathanaël Labreuil <nathanael.labreuil@gmail.com>
2022-03-27 17:52:07 +0200

    chore: Add display and body fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
 	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				display: ["Space Grotesk", ...defaultTheme.fontFamily.serif],
+				body: ["IBM Plex Sans", ...defaultTheme.fontFamily.sans],
+			},
+		},
 	},
 	plugins: [],
 };
