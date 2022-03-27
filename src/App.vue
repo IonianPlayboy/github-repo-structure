2850b029f23a9044eb619c8c262d881e085f66a0
Nathanaël Labreuil <nathanael.labreuil@gmail.com>
2022-03-27 23:44:31 +0200

    style(app): add body styles

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
+	<Head>
+		<body class="min-h-screen bg-gray-900 font-body text-gray-300" />
+	</Head>
 	<AppHeader />
 	<RouterView />
 </template>
 
 <script setup lang="ts">
+import { Head } from "@vueuse/head";
 import { RouterView } from "vue-router";
 import AppHeader from "@/components/organisms/AppHeader.vue";
 </script>
